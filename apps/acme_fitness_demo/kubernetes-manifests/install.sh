e0fceae594511be9b546598384f42e37fcdd5d2a
atozprasad <atozcareer@gmail.com>
2021-12-14 20:12:29 -0600

    Updated to user Perisstent volumes

--- apps/acme_fitness_demo/kubernetes-manifests/install.sh
+++ apps/acme_fitness_demo/kubernetes-manifests/install.sh
@@ -3,6 +3,7 @@ ACME_SECRET=VMware1!
 NAMESPACE=acme-fitness
 
 kubectl create ns ${NAMESPACE}
+kubectl -n ${NAMESPACE} apply -f 01-create-pvc.yaml
 kubectl -n ${NAMESPACE} create secret generic cart-redis-pass --from-literal=password=${ACME_SECRET}
 kubectl -n ${NAMESPACE} apply -f cart-redis-total.yaml
 kubectl -n ${NAMESPACE} apply -f cart-total.yaml
@@ -22,18 +23,23 @@ kubectl -n ${NAMESPACE} apply -f users-redis-total.yaml
 kubectl -n ${NAMESPACE} apply -f users-total.yaml
 kubectl -n ${NAMESPACE} apply -f frontend-total.yaml
 kubectl -n ${NAMESPACE} apply -f point-of-sales-total.yaml
+kubectl -n ${NAMESPACE} apply -f point-of-sales-total.yaml
+
+####  LB & Route objects
+oc -n ${NAMESPACE} apply -f acme-lb.yaml
+oc -n ${NAMESPACE} apply -f route-ingress.yaml
 #kubectl -n ${NAMESPACE} apply -f https-secret.yaml
 #kubectl -n ${NAMESPACE} apply -f https-ingress-overnp.yaml
 
 ## Apply SCC to the serviceaccounts/users default, deployer, builder
 echo -e "\n\t Applying  SCC to the serviceaccounts/users default, deployer, builder"
 
-oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z deployer
-oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z builder
-oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z default
 
 oc adm policy add-scc-to-user anyuid -n ${NAMESPACE}  -z deployer
 oc adm policy add-scc-to-user anyuid -n ${NAMESPACE} -z builder
 oc adm policy add-scc-to-user anyuid -n ${NAMESPACE} -z default
+oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z deployer
+oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z builder
+oc adm policy add-scc-to-user privileged -n ${NAMESPACE} -z default
 
 kubectl config set-context --current --namespace=${NAMESPACE}
